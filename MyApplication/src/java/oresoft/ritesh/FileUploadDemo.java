21f49e1d176ec3a4a537a2074e2acb037eef730d
ritesh.uml <ritesh.uml@97de11f7-d9f5-8ce0-ae5a-507b4682e62f>
2010-04-02 07:04:40 +0000

    This this the Program written by Ritesh

---
+++ MyApplication/src/java/oresoft/ritesh/FileUploadDemo.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oresoft.ritesh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.commons.fileupload.DiskFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.fileupload.FileUploadException;
+/**
+ *
+ * @author Ritesh Kumar
+ */
+public class FileUploadDemo extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+           FileUpload fup=new FileUpload();
+           boolean isMultipart=FileUpload.isMultipartContent(request);
+           DiskFileUpload upload=new DiskFileUpload();
+           List /* FileItem */ items=upload.parseRequest(request);
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FileUploadDemo</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FileUploadDemo at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+           
+        }
+        finally {
+            out.close();
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ MyApplication/src/java/oresoft/ritesh/RandomLinkToSites.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oresoft.ritesh;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+
+/**
+ *
+ * @author Ritesh Kumar
+ */
+public class RandomLinkToSites extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    Vector sites = new Vector();
+  Random random = new Random();
+ public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    sites.addElement(" http://www.oracle.com");
+    sites.addElement("http://www.ibm.com");
+    sites.addElement("http://www.google.com");
+    sites.addElement("http://www.yahoo.com");
+     sites.addElement("http://www.cricinfo.com");
+  }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+         response.setContentType("text/html;charset=UTF-8");
+        try {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RandomLinkToSites</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            int siteIndex = Math.abs(random.nextInt()) % sites.size();
+    String site = (String)sites.elementAt(siteIndex);
+    response.setStatus(response.SC_MOVED_TEMPORARILY);
+    response.setHeader("Location", site);
+            //out.println("<h1>Servlet RandomLinkToSites at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            } finally { 
+            out.close();
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
